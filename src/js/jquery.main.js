517a4df7258e159be9ea6b47b85500b4a1d8fe97
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-07-05 11:16:13 +0300

    Add programm

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	// Tabs
 		$('[data-action="tab"]').click(function(){			
 			// Tab links toggle class
-				$(this).closest(".vtabs__list").children("li").removeClass('active');
+				$(this).closest(".tabs__list").children("li").removeClass('active');
 				$(this).parent().addClass('active');
 			// Show tab content
 				var tabTarget = $(this).attr('data-target');
-				$(tabTarget).fadeIn('slow');
-				$(".vtabs__content > div").not($(tabTarget)).fadeOut('slow');
+				$(tabTarget).fadeIn(0);
+				$(".tabs__content > div").not($(tabTarget)).fadeOut(0);
 		});
 	// Develope
 });	
